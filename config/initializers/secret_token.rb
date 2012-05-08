c1795783cb64b53dca3268bf02d56a9c78b92ecd
Josh Dinges <jdinges@mines.edu>
2012-05-08 14:21:27 -0600

    first boiler plate commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = 'd0290c574efe0f8b42a8f34e36a7d75ccc3ce381b47769291579a0766d0991639d504c80a2043d7665eaf2da43e9d91e61f166f9e8353607e23c5c9cab5547df'
